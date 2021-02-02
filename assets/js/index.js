4fda788ccf4a6c51ecf191255297d7087ceb8b4a
NuwanJ <nuwanjaliyagoda@gmail.com>
2021-02-02 13:04:08 +0530

    js event fix

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
   window.onload = (event) => {
     $(".preloader").fadeOut("fast");
   };
+  setTimeout(function () {
+    $(".preloader").fadeOut("fast");
+  }, 2500);
 });
