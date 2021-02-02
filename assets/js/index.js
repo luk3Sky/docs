129c229d2252d18ad7e601fb3fded6eef725fcda
NuwanJ <nuwanjaliyagoda@gmail.com>
2021-02-02 12:38:20 +0530

    Smooth page loading feature

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,5 @@
-// $(window).load(function () {
-//   $(".preloader").fadeOut("slow");
-// });
-//
 $(document).ready(function () {
-  setTimeout(function () {
-    $(".preloader").fadeOut(1000, "swing");
-  }, 5000);
+  window.onload = (event) => {
+    $(".preloader").fadeOut("fast");
+  };
 });
